01606a0d71de56d9a4ec0c26d698ddfe2f75fe4a
qkrmekem <magicofclown@naver.com>
2023-02-18 23:17:37 +0900

    AppConfig 개발(관심사의 분리)

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, "구현 객체를 생성"하고, "연결"하는 책임을
+// 가지는 별도의 클래스
+public class AppConfig {
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+    public OrderService orderService(){
+        return new OrderServiceImpl(
+                new MemoryMemberRepository(),
+                new FixDiscountPolicy());
+    }
+}
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,7 +4,8 @@ import hello.core.member.*;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appconfig = new AppConfig();
+        MemberService memberService = appconfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
