9816b54e2300fb515dbc4a35107c247856f1a66d
qkrmekem <magicofclown@naver.com>
2023-02-25 22:11:30 +0900

    싱글톤 패턴 유지 확인

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,20 +15,39 @@ import org.springframework.context.annotation.Configuration;
 // 가지는 별도의 클래스
 @Configuration
 public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    //싱글톤이 깨지는건 아닐까??
+
+    //예상
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //실제
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //=> 싱글톤 패턴 유지
+
     @Bean
     public MemberService memberService(){
-
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
-
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -2,15 +2,17 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price;
+    // 이렇게 되면 price가 공유필드가 되어버려 문제가 발생함!
+//    private int price;
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 여기가 문제!!
-    }
-
-    public int getPrice(){
+//        this.price = price; // 여기가 문제!!
         return price;
     }
+
+//    public int getPrice(){
+//        return price;
+//    }
 
 }
