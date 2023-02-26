5cb82b723947695656f55fe1996477900cc398bd
qkrmekem <magicofclown@naver.com>
2023-02-27 03:10:06 +0900

    컴포넌트 스캔

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
     // 하나의 메소드는 하나의 역할만 해야함
     // 할인 값을 구하는 메서드이므로 할인 값만 구해야함
